ed338a613432e589d7388590818585ed8524ac66
Bamboozled <64771089+BamBoozledMC@users.noreply.github.com>
2021-08-12 10:52:03 +1000

    no idea

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-console.log('Hello World!');
+var ping = require ("net-ping");
 
-response = readline()
+var session = ping.createSession ();
+
+
+session.pingHost ("bamboozledmc.xyz", function (error, target) {
+    if (error)
+        if (error instanceof ping.RequestTimedOutError)
+            console.log (target + ": Not alive");
+        else
+            console.log (target + ": " + error.toString ());
+    else
+        console.log (target + ": Alive");
+});
+
+
+console.log('Hello World!');
